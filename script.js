60008617f33aaa976a9249f1497cf61d4aa29959
Praveensrinivasan01 <102655015+Praveensrinivasan01@users.noreply.github.com>
2022-04-21 16:57:44 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,180 @@
+let n=0
+    let num1=50,num2=30,num3=25,num4=15,num5=10;
+    var total=0
+    function button1(){
+        if(n<1)     
+        {  
+            n++
+        
+            total += num1
+           
+        
+        
+        console.log("t1",total)
+       
+        var b = document.getElementById("pay");
+        b.innerHTML=total;
+        
+        
+        
+       
+    let a = document.createElement('p1')
+    a.innerHTML='Patty...&#8377; 50'
+    document.getElementById('fieldset').appendChild(a)
+
+
+   
+    let aa = document.getElementById('burger1')
+    aa.style.backgroundColor='brown'
+    aa.style.height='10px'
+    aa.style.width= '100px'
+    aa.style.borderRadius ='4px'
+    aa.style.margin = 'auto'
+    document.getElementById('div300').appendChild(aa)
+}}
+let s=0
+  function thank(){
+      if(s<1)
+      {
+          s++
+    let l = document.createElement('p')
+    l.innerHTML='YOUR ORDER HAS BEEN BOOKED!!<br><br>THANK YOU'
+    
+    document.getElementById('fieldset100').appendChild(l)
+    l.style.backgroundColor = 'white'
+    l.style.height='150px'
+    l.style.display='flex'
+    l.style.alignItems='center'
+    l.style.width='350px'
+    l.style.border='7px solid yellow'
+
+  }  }
+
+
+let j=0
+function button2(){
+
+    if(j<1)
+    {
+        j++
+    
+        total += num2
+    
+    console.log("t2",total)
+   
+    var c = document.getElementById("pay");
+    c.innerHTML=total;
+
+  
+    let b = document.createElement('p')
+    b.innerHTML='Cheese....&#8377; 30'
+    document.getElementById('fieldset').appendChild(b)
+
+   
+    let bb = document.getElementById('burger2')
+    bb.style.backgroundColor='yellow'
+    bb.style.height='10px'
+    bb.style.width= '100px'
+    bb.style.borderRadius ='4px'
+    bb.style.margin = 'auto'
+    document.getElementById('div300').appendChild(bb)
+}
+}
+let k=0
+function button3(){
+    if(k<1)
+    {
+        k++
+    
+        total += num3
+    
+    console.log("t3",total)
+   
+    var d = document.getElementById("pay");
+    d.innerHTML=total;
+   
+    let c = document.createElement('p')
+    c.innerHTML='Tomatoes...&#8377; 25'
+    document.getElementById('fieldset').appendChild(c)
+    
+    let cc = document.getElementById('burger3')
+    cc.style.backgroundColor='red'
+    cc.style.height='10px'
+    cc.style.width= '100px'
+    cc.style.borderRadius ='4px'
+    cc.style.margin = 'auto'
+    document.getElementById('div300').appendChild(cc)
+}
+}
+let l=0
+function button4(){
+    if(l<1){
+        l++
+   
+        total += num4
+    
+    console.log("t4",total)
+    
+    var c = document.getElementById("pay");
+    c.innerHTML=total;
+    let d = document.createElement('p')
+    d.innerHTML='onion...&#8377; 15' 
+    document.getElementById('fieldset').appendChild(d)
+   
+    let dd = document.getElementById('burger4')
+    dd.style.backgroundColor='white'
+    dd.style.height='10px'
+    dd.style.width= '100px'
+    dd.style.borderRadius ='4px'
+    dd.style.margin = 'auto'
+    document.getElementById('div300').appendChild(dd)
+}
+}
+let m=0
+function button5(){
+    if(m<1){
+        m++
+    
+    
+        total += num5
+    
+    console.log("t5",total)
+    
+    var c = document.getElementById("pay");
+    c.innerHTML=total;
+   
+    let e = document.createElement('p')
+    e.innerHTML='lettuce...&#8377; 10'
+    document.getElementById('fieldset').appendChild(e)
+  
+     let dd = document.getElementById('burger5')
+    dd.style.backgroundColor='green'
+    dd.style.height='10px'
+    dd.style.width= '100px'
+    dd.style.borderRadius ='4px'
+    dd.style.margin = 'auto'
+    document.getElementById('div300').appendChild(dd)
+}
+}
+
+let ff = document.getElementById('burger0')
+ff.style.backgroundColor='#ffcc00'
+ff.style.height='20px'
+ff.style.width= '100px'
+ff.style.borderRadius ='4px'
+ff.style.borderBottomRightRadius='20px'
+ff.style.borderBottomLeftRadius='20px'
+    document.getElementById('div200').appendChild(ff)
+
+    let gg = document.getElementById('burger6')
+gg.style.backgroundColor='#ffcc00'
+gg.style.height='40px'
+gg.style.width= '100px'
+gg.style.borderRadius ='4px'
+gg.style.borderTopRightRadius='90px'
+gg.style.borderTopLeftRadius='90px'
+    document.getElementById('div100').appendChild(gg)
+
+
+   
+  
